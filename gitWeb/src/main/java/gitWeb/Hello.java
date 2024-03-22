c882274842ecb7070920bf5fcebab49d50980b17
jeongmin <'jeongmin0521@naver.com>
2024-03-22 15:14:13 +0900

    리셋

--- gitWeb/src/main/java/gitWeb/Hello.java
+++ gitWeb/src/main/java/gitWeb/Hello.java
@@ -4,9 +4,14 @@ public class Hello {
 	public static void main(String [] args) {
 		System.out.println("hello world");
 		//구글 사용자 마성일이 추가한 기능 
+
+		//정미니가 추가한 기능
+
 		
 		//민영이 추가한 기능
 		
 		//오후 화이팅 추가 02:30
+
 	}
+	//충돌인가요?
 }
